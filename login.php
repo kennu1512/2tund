57b97e64596cec723e94ba8a221c51d5489c78d7
kenkool <kenkool@greeny.cs.tlu.ee>
2016-09-15 12:23:27 +0300

    login

--- login.php
+++ login.php
@@ -7,17 +7,42 @@
 
           <h1>Logi sisse</h1>
          
-		 <form>
+		 <form method="POST">
 		 
-		     <input type="email">
+		     <input placeholder="E-post" name="loginEmail" type="email">
 		     
 			 <br><br>
 			 
-		     <input type="password">
+		     <input placeholder="parool" name="loginPassword" type="password">
+             
 			 
 			 <br><br>
 			 
 			 <input type="submit">
+			 
+		 </form>
+
+     </body>
+</html>
+
+
+
+ <h1>Loo kasutaja</h1>
+         
+		 <form method="POST">
+		 
+		 
+		      
+		     <input placeholder="E-post" name="signupEmail" type="email">
+		     
+			 <br><br>
+			 
+		     <input placeholder="parool" name="signupPassword" type="password">
+             
+			 
+			 <br><br>
+			 
+			 <input type="submit" value="Loo kasutaja">
 			 
 		 </form>
 
